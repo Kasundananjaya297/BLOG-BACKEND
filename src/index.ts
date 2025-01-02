ae001d66d105f047ee4bcde260c40aedddb5f4b5
Kasun Dhanajaya <dhananj-im20045@stu.kln.ac.lk>
2025-01-02 13:30:47 +0530

    Add dotenv support, update server configuration, and implement 404 error page

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,32 @@
 import express from 'express';
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
-const PORT = 3300;
+const PORT = process.env.PORT || 3000;
+
+/* Middlewares  to parse JSON bodies*/
+app.use(express.json());
+
+// Middleware to parse URL-encoded bodies
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', async(req, res) => {
+    res.send('Server is running');
+});
+app.get('/info', async(req, res) => {
+    res.send({
+        PORT: PORT,
+        NODE_ENV: process.env.ENV,
+        DATABASE_URL: process.env.DB_URL,
+    });
+});
+app.use((req, res, next) => {
+    //extract the path from the request
+    const path = req.path;
+    res.status(404).sendFile(__dirname + "/views/index.html");
+  });
 
 app.listen(PORT, () => {
     console.log(`Server is Successfully Running, and App is listening on port ${PORT}`);
